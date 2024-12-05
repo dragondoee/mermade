2b2cae507c4385ebb5687210b90100e37ff2dcc7
Meylie <emilie.desgranges78@gmail.com>
2024-12-06 00:58:56 +0100

    Affiche des sections
    Les boutons sont interactifs, on permet de changer les sections afficher

--- game.js
+++ game.js
@@ -9,14 +9,42 @@
 
 // ================================================ Changement de section ======================================================
 
-// Affiche Sirène
-// Cache Sirène
+// Section
+const divSirene = document.querySelector(".sirene")
+const divSituation = document.querySelector(".situation")
+const divConsequence = document.querySelector(".consequence")
 
-// Affiche situation
-// Cache situation
 
-// Affiche conséquence
-// Cache conséquence
+var boutonsPartie = document.querySelectorAll(".part")
+// Cache Sirène + Affiche situation
+boutonsPartie.forEach(function (boutonPartie){
+    boutonPartie.addEventListener("click", function () {
+        console.log(boutonPartie.innerHTML);
+        divSirene.style.display="none"
+        divSituation.style.display="block"
+    });
+});
+
+
+
+var boutonsChoix = document.querySelectorAll(".button-choice")
+// Cache situation + Affiche conséquence
+boutonsChoix.forEach(function (boutonChoix){
+    boutonChoix.addEventListener("click", function () {
+        console.log(boutonChoix.innerHTML);
+        divSituation.style.display="none"
+        divConsequence.style.display="block"
+    });
+});
+
+
+var boutonSuivant = document.querySelector(".next-game button")
+// Cache conséquence + Affiche Sirène
+boutonSuivant.addEventListener("click", function () {
+        console.log("bananana");
+        divConsequence.style.display="none"
+        divSirene.style.display="block"
+    });
 
 // ================================================ Système de choix ==========================================================
 
