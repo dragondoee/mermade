fb9eecdb671341110fa5260ed1e3b8517ed1d273
Meylie <emilie.desgranges78@gmail.com>
2024-12-06 03:05:49 +0100

    choix

--- game.js
+++ game.js
@@ -1,4 +1,94 @@
 
+"use strict"
+
+fetch('game-data.json').then(function (response) {
+    response.json().then(function (data) {
+
+
+
+
+
+        // ================================================ Changement de section ======================================================
+
+        var index = 0
+
+        // Section
+        const divSirene = document.querySelector(".sirene")
+        const divSituation = document.querySelector(".situation")
+        const divConsequence = document.querySelector(".consequence")
+
+        // Boutons
+        var boutonsPartie = document.querySelectorAll(".sirene button")
+        var boutonsChoix = document.querySelectorAll(".button-choice")
+        var boutonSuivant = document.querySelector(".next-game button")
+
+        // Partie
+        var partie = data
+        var choix = data
+
+
+        // Cache Sirène + Affiche situation
+        boutonsPartie.forEach(function (boutonPartie) {
+            boutonPartie.addEventListener("click", function () {
+                // Si le bouton est actif
+                if (boutonPartie.classList.contains("active")) {
+                    divSirene.style.display = "none"
+                    divSituation.style.display = "block"
+                }
+                data.forEach(function (part) {
+                    if (boutonPartie.classList.contains(part.part)) {
+                        partie = part
+                        console.log(part.part)
+                        // Afficher la situation et les différents choix
+                        document.querySelector(".situation img").src = "img/situation/" + part.part + ".jpg"
+                        document.querySelector(".situation .txt-game").innerHTML = part.situation
+                        part.choices.forEach(function () {
+                            boutonsChoix[index].innerHTML = part.choices[index].choice
+                            index++
+                        })
+
+                    }
+                })
+
+            });
+
+
+        });
+
+
+
+
+        // Cache situation + Affiche conséquence
+        boutonsChoix.forEach(function (boutonChoix) {
+            boutonChoix.addEventListener("click", function () {
+                choix = boutonChoix.classList[1]
+                console.log(choix)
+                divSituation.style.display = "none"
+                divConsequence.style.display = "block"
+                // Afficher la conséquence ( texte + image + gain de points)
+                document.querySelector(".consequence .txt-game").innerHTML = part.choices
+            });
+        });
+
+
+
+        // Cache conséquence + Affiche Sirène
+        boutonSuivant.addEventListener("click", function () {
+            divConsequence.style.display = "none"
+            divSirene.style.display = "block"
+            // Mettre à jour le status de la partie ( bien ou mal )
+
+            // Désactiver le bouton de la partie
+            // if (boutonPartie.classList.contains("partie actuelle")) {
+            //     boutonPartie.classList.remove("active")
+            // Changer son style selon son status
+            // }
+
+        });
+
+        // ================================================ Système de choix ==========================================================
+
+        // ================================================== Système de point ========================================================
 
 
 
@@ -7,73 +97,9 @@
 
 
 
-// ================================================ Changement de section ======================================================
-
-// Section
-const divSirene = document.querySelector(".sirene")
-const divSituation = document.querySelector(".situation")
-const divConsequence = document.querySelector(".consequence")
 
 
-var boutonsPartie = document.querySelectorAll(".sirene button")
-// Cache Sirène + Affiche situation
-boutonsPartie.forEach(function (boutonPartie) {
-    boutonPartie.addEventListener("click", function () {
-        console.log(boutonPartie.classList.contains("active"))
-        // Si le bouton est actif
-        if (boutonPartie.classList.contains("active")) {
-            console.log(boutonPartie.innerHTML);
-            divSirene.style.display = "none"
-            divSituation.style.display = "block"
-        }
-        // Afficher la situation et les différents choix
+
+
     });
-
-
 });
-
-
-
-var boutonsChoix = document.querySelectorAll(".button-choice")
-// Cache situation + Affiche conséquence
-boutonsChoix.forEach(function (boutonChoix) {
-    boutonChoix.addEventListener("click", function () {
-        console.log(boutonChoix.innerHTML);
-        divSituation.style.display = "none"
-        divConsequence.style.display = "block"
-        // Afficher la conséquence ( texte + image + gain de points)
-    });
-});
-
-
-var boutonSuivant = document.querySelector(".next-game button")
-// Cache conséquence + Affiche Sirène
-boutonSuivant.addEventListener("click", function () {
-    console.log("bananana");
-    divConsequence.style.display = "none"
-    divSirene.style.display = "block"
-    // Mettre à jour le status de la partie ( bien ou mal )
-    
-    // Désactiver le bouton de la partie
-    // if (boutonPartie.classList.contains("partie actuelle")) {
-    //     boutonPartie.classList.remove("active")
-        // Changer son style selon son status
-    // }
-
-});
-
-// ================================================ Système de choix ==========================================================
-
-// ================================================== Système de point ========================================================
-
-
-
-
-
-
-
-
-
-
-
-
