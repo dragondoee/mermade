6f5dd0477da351db2792e7bdb7370820de3188f5
Meylie <emilie.desgranges78@gmail.com>
2024-12-06 01:52:38 +0100

    Bouton partie actif

--- game.js
+++ game.js
@@ -15,25 +15,33 @@ const divSituation = document.querySelector(".situation")
 const divConsequence = document.querySelector(".consequence")
 
 
-var boutonsPartie = document.querySelectorAll(".part")
+var boutonsPartie = document.querySelectorAll(".sirene button")
 // Cache Sirène + Affiche situation
-boutonsPartie.forEach(function (boutonPartie){
+boutonsPartie.forEach(function (boutonPartie) {
     boutonPartie.addEventListener("click", function () {
-        console.log(boutonPartie.innerHTML);
-        divSirene.style.display="none"
-        divSituation.style.display="block"
+        console.log(boutonPartie.classList.contains("active"))
+        // Si le bouton est actif
+        if (boutonPartie.classList.contains("active")) {
+            console.log(boutonPartie.innerHTML);
+            divSirene.style.display = "none"
+            divSituation.style.display = "block"
+        }
+        // Afficher la situation et les différents choix
     });
+
+
 });
 
 
 
 var boutonsChoix = document.querySelectorAll(".button-choice")
 // Cache situation + Affiche conséquence
-boutonsChoix.forEach(function (boutonChoix){
+boutonsChoix.forEach(function (boutonChoix) {
     boutonChoix.addEventListener("click", function () {
         console.log(boutonChoix.innerHTML);
-        divSituation.style.display="none"
-        divConsequence.style.display="block"
+        divSituation.style.display = "none"
+        divConsequence.style.display = "block"
+        // Afficher la conséquence ( texte + image + gain de points)
     });
 });
 
@@ -41,10 +49,18 @@ boutonsChoix.forEach(function (boutonChoix){
 var boutonSuivant = document.querySelector(".next-game button")
 // Cache conséquence + Affiche Sirène
 boutonSuivant.addEventListener("click", function () {
-        console.log("bananana");
-        divConsequence.style.display="none"
-        divSirene.style.display="block"
-    });
+    console.log("bananana");
+    divConsequence.style.display = "none"
+    divSirene.style.display = "block"
+    // Mettre à jour le status de la partie ( bien ou mal )
+    
+    // Désactiver le bouton de la partie
+    // if (boutonPartie.classList.contains("partie actuelle")) {
+    //     boutonPartie.classList.remove("active")
+        // Changer son style selon son status
+    // }
+
+});
 
 // ================================================ Système de choix ==========================================================
 
